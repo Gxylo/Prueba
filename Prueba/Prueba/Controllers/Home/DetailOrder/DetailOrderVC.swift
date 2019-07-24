69bfff8b351508501e8ff0a6f9efe5ffbea84c93
xyloPark <xylopark@gmail.com>
2019-07-23 19:46:15 -0500

    Bug fix - Detalle de ordén
    Se agrega el botón para regresar a la vista de home(Listado de ordenes)

--- Prueba/Prueba/Controllers/Home/DetailOrder/DetailOrderVC.swift
+++ Prueba/Prueba/Controllers/Home/DetailOrder/DetailOrderVC.swift
@@ -40,6 +40,12 @@ class DetailOrderVC: UIViewController {
         
         
     }
+    
+    @IBAction func backToView(_ sender: Any) {
+        
+        self.navigationController?.popViewController(animated: true)
+    }
+    
     /*
     // MARK: - Navigation
 
